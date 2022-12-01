980b40a03ce3ff56cf3edf04c040a52210f7c667
whitequark <whitequark@whitequark.org>
2022-12-01 20:01:37 +0000

    Deploying to main from @ amaranth-lang/amaranth@0a1ba2205064512455e9e9d1ace1b63f0bffffc8 🚀

--- docs/amaranth/latest/_static/documentation_options.js
+++ docs/amaranth/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev34',
+    VERSION: '0.4.dev35',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
